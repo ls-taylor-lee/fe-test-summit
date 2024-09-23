96878ec046caf442390880a7ad56352336ead162
Taylor <117108016+ls-taylor-lee@users.noreply.github.com>
2024-09-22 22:37:41 -0400

    min height fix

--- front/src/app/page.tsx
+++ front/src/app/page.tsx
@@ -62,7 +62,7 @@ export default function Home() {
   if (error) return <div> Error fetching data </div>;
 
   return (
-    <div className="p-4">
+    <div className="p-4 min-h-screen">
       <div className="flex mb-4 items-center">
         <h1 className="text-2xl font-bold">All Drinks</h1>
         <button className="text-black dark:text-white px-4 py-2 rounded" onClick={() => openModalForAdd()}>
